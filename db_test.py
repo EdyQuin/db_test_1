7087f7995bba3b4c8d7393314b9b0f2c5260bb61
Eduardo Quin <94932412+EdyQuin@users.noreply.github.com>
2022-09-24 16:37:46 -0400

    Update db_test.py

--- db_test.py
+++ db_test.py
@@ -25,7 +25,7 @@ if submitted:
 "Thank you. Your electronic will is now securely stored."
 # This reads all items from the database and displays them to your app.
 # db_content is a list of dictionaries. You can do everything you want with it.
-db_content = db.fetch()
+db_content = db.fetch(query=None, limit=None, last=None).items
 st.write(db_content)
 
 st.image('./LOGO_091622.png')
